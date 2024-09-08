903bd91390cfa2ece6425862a9091b91342f3efd
Bruno Silva <brunosilva9725@gmail.com>
2024-09-08 19:49:23 -0300

    Seller entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,12 +1,18 @@
 package app;
 
-import model.Department;
+import java.util.Date;
+
+import entities.Department;
+import entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         
         Department obj = new Department(1, "Books");
 
+        Seller seller = new Seller(21, "Bruno", "bruno@gmail.com", new Date(), 3000.00, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
