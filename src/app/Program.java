b2f4f3bae0367cdc46321466c05a0ccf2acd8bdf
Bruno Silva <brunosilva9725@gmail.com>
2024-09-08 19:37:29 -0300

    Department entity class

---
+++ src/app/Program.java
@@ -0,0 +1,12 @@
+package app;
+
+import model.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
+    }
+}
